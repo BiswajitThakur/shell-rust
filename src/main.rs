db00f22f65ce8451d67fcecb6b791f66871dccaf
BiswajitThakur <biswajit733127@gmail.com>
2024-12-27 19:58:00 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{self, BufReader, BufWriter, Read, Write};
+use std::io::{self, BufWriter, Write};
 use std::iter::{Enumerate, Peekable};
 use std::process::Stdio;
 use std::str::Chars;
@@ -32,7 +32,6 @@ enum Cmd<'a> {
     Type(Cow<'a, str>),
     Pwd,
     Cd(Cow<'a, str>),
-    Cat(Vec<Cow<'a, str>>),
     Other(Cow<'a, str>, Vec<Cow<'a, str>>),
 }
 
@@ -44,7 +43,6 @@ impl fmt::Display for Cmd<'_> {
             Self::Type(_) => f.write_str("type")?,
             Self::Pwd => f.write_str("pwd")?,
             Self::Cd(_) => f.write_str("cd")?,
-            Self::Cat(_) => f.write_str("cat")?,
             Self::Other(cmd, _) => {
                 if let Some(path) = find_path(cmd) {
                     return write!(f, "{} is {}", cmd, path);
@@ -59,11 +57,12 @@ impl fmt::Display for Cmd<'_> {
 
 impl Cmd<'_> {
     fn is_builtin(&self) -> bool {
-        !matches!(self, Self::Other(_, _) | Self::Cat(_))
+        !matches!(self, Self::Other(_, _))
     }
 }
 
 impl<'a> Cmd<'a> {
+    #[allow(unused)]
     fn execute(&'a self, out: Redirection<'_>) -> io::Result<()> {
         let mut stdout = BufWriter::new(out.stdout()?);
         let mut stderr = BufWriter::new(out.stderr()?);
@@ -107,39 +106,10 @@ impl<'a> Cmd<'a> {
                     writeln!(stdout, "cd: {}: No such file or directory", path)?;
                 }
             }
-            Self::Cat(args) => {
-                for path in args {
-                    let path = match path {
-                        Cow::Borrowed(v) => *v,
-                        Cow::Owned(v) => v,
-                    };
-                    let file = match fs::File::open(path) {
-                        Ok(f) => f,
-                        _ => {
-                            stderr.write("cat: ".as_bytes()).unwrap();
-                            stderr.write(path.as_bytes()).unwrap();
-                            stderr
-                                .write(": No such file or directory\n".as_bytes())
-                                .unwrap();
-                            stderr.flush().unwrap();
-                            continue;
-                        }
-                    };
-                    let mut reader = BufReader::new(file);
-                    let mut buffer: [u8; 1024] = [0; 1024];
-                    loop {
-                        let r = reader.read(&mut buffer)?;
-                        if r == 0 {
-                            break;
-                        }
-                        stdout.write_all(&buffer[..r])?;
-                    }
-                }
-            }
             Self::Other(cmd, args) => {
-                if let Some(path) = find_path(cmd) {
-                    let mut child = process::Command::new(path)
-                        .arg(args.join(" "))
+                if find_path(cmd).is_some() {
+                    let mut child = process::Command::new(cmd.as_ref())
+                        .args(args.iter().map(|v| v.as_ref()).collect::<Vec<&str>>())
                         .stdout(Stdio::from(out.stdout()?))
                         .stderr(Stdio::from(out.stderr()?))
                         .spawn()?;
@@ -167,7 +137,6 @@ impl<'a> From<&'a str> for Cmd<'a> {
             "type" => Self::Type(cmd_args.next().unwrap_or_default()),
             "pwd" => Self::Pwd,
             "cd" => Self::Cd(cmd_args.next().unwrap_or(Cow::Borrowed("~"))),
-            "cat" => Self::Cat(cmd_args.collect()),
             _ => Self::Other(cmd, cmd_args.collect()),
         }
     }
@@ -185,7 +154,6 @@ impl<'a> From<Vec<Cow<'a, str>>> for Cmd<'a> {
             "type" => Self::Type(iter.next().unwrap_or_default()),
             "pwd" => Self::Pwd,
             "cd" => Self::Cd(iter.next().unwrap_or(Cow::Borrowed("~"))),
-            "cat" => Self::Cat(iter.collect()),
             _ => Self::Other(cmd, iter.collect()),
         }
     }
@@ -255,8 +223,8 @@ fn remove_unwanted(value: &str, remove: Vec<usize>) -> Cow<'_, str> {
         start += 1;
     }
     let mut end = value.len() - 1;
-    for i in remove.len() - 1..0 {
-        if remove[i] != end {
+    for item in remove.iter().rev() {
+        if *item != end {
             break;
         }
         end -= 1;
@@ -391,39 +359,42 @@ impl Redirection<'_> {
         match self.std_err.ops {
             RedirOps::Append => Ok(fs::OpenOptions::new()
                 .append(true)
+                .create(true)
                 .open(self.std_err.path.as_ref())?),
             RedirOps::Redirect => Ok(fs::File::create(self.std_err.path.as_ref())?),
         }
     }
 }
 
-fn get_redirect_path<'a>(
-    args: Vec<Cow<'a, str>>,
-) -> io::Result<(Redirection<'a>, Vec<Cow<'a, str>>)> {
+fn get_redirect_path(args: Vec<Cow<'_, str>>) -> io::Result<(Redirection<'_>, Vec<Cow<'_, str>>)> {
     let mut args1 = Vec::with_capacity(args.len());
-    let mut iter = args.into_iter().peekable();
+    let mut iter = args.into_iter();
     let mut stdout_path = None;
-    let mut stdout_ops = None;
+    let mut stdout_ops = RedirOps::Append;
     let mut stderr_path = None;
-    let mut stderr_ops = None;
+    let mut stderr_ops = RedirOps::Append;
     while let Some(arg) = iter.next() {
         match arg.as_ref() {
             ">" | "1>" => {
                 if stdout_path.is_none() {
                     stdout_path = iter.next();
-                    stdout_ops = Some(RedirOps::Redirect);
+                    stdout_ops = RedirOps::Redirect;
                 }
             }
             ">>" | "1>>" => {
                 if stderr_path.is_none() {
                     stdout_path = iter.next();
-                    stdout_ops = Some(RedirOps::Append);
                 }
             }
             "2>" => {
                 if stderr_path.is_none() {
                     stderr_path = iter.next();
-                    stderr_ops = Some(RedirOps::Redirect);
+                    stderr_ops = RedirOps::Redirect;
+                }
+            }
+            "2>>" => {
+                if stderr_path.is_none() {
+                    stderr_path = iter.next();
                 }
             }
             _ => args1.push(arg),
@@ -433,177 +404,13 @@ fn get_redirect_path<'a>(
         Redirection {
             std_out: RedirectPath {
                 path: stdout_path.unwrap_or(Cow::Borrowed("/dev/stdout")),
-                ops: stdout_ops.unwrap_or(RedirOps::Append),
+                ops: stdout_ops,
             },
             std_err: RedirectPath {
                 path: stderr_path.unwrap_or(Cow::Borrowed("/dev/stderr")),
-                ops: stderr_ops.unwrap_or(RedirOps::Append),
+                ops: stderr_ops,
             },
         },
         args1,
     ))
-}
-
-#[cfg(test)]
-mod tests {
-    use std::borrow::Cow;
-
-    use crate::{handle_args, remove_unwanted, Cmd, IterArgs};
-
-    fn test_fn_handle_ukt(iter: Vec<(&str, Vec<usize>, usize)>) {
-        for (index, (input, rm, end)) in iter.into_iter().enumerate() {
-            let mut e = 0;
-            let mut remove = Vec::new();
-            handle_args(
-                &mut input.chars().enumerate().peekable(),
-                &mut remove,
-                &mut e,
-            );
-            if rm != remove || end != e {
-                eprintln!("Test Index: {}\nInput: '{}'", index, input);
-                eprintln!("want: '{:?}', got: '{:?}'", &rm, &remove);
-                eprintln!("want: '{}', got: '{}'", end, e);
-            }
-            assert_eq!(rm, remove);
-            assert_eq!(end, e);
-        }
-    }
-
-    #[test]
-    fn test_handle_args() {
-        let test = vec![
-            ("", vec![], 0),
-            (" ", vec![0], 1),
-            ("  ", vec![0], 1),
-            ("     ", vec![0], 1),
-            (r#""abc""#, vec![0, 4], 5),
-            (r#"'abc'"#, vec![0, 4], 5),
-            (r#""ab   c""#, vec![0, 7], 8),
-            (r#"'ab   c'"#, vec![0, 7], 8),
-            ("abc def", vec![3], 4),
-            ("abc def ghi", vec![3], 4),
-            (r#"'"'"#, vec![0, 2], 3),
-            (r#""'""#, vec![0, 2], 3),
-            (r#""\"""#, vec![0, 1, 3], 4),
-            (r#"abc\ndef\ efg\txyz"#, vec![3, 8, 13], 18),
-            (r#"\\\\\\\\"#, vec![0, 2, 4, 6], 8),
-            (r#"hello\ "#, vec![5], 7),
-            (r#"\ hello"#, vec![0], 7),
-            (r#"hello\     "#, vec![5, 7], 8),
-            (r#""abc""def""#, vec![0, 4, 5, 9], 10),
-            (r#"'abc''def'"#, vec![0, 4, 5, 9], 10),
-            (r#"'abc'"def""#, vec![0, 4, 5, 9], 10),
-            (r#""abc"'def'"#, vec![0, 4, 5, 9], 10),
-            (r#""abc" 'def'"#, vec![0, 4, 5], 6),
-        ];
-        test_fn_handle_ukt(test);
-    }
-    fn test_fn_rm_unwanted(iter: Vec<(&str, Vec<usize>, &str, bool)>) {
-        for (index, (input, remove, want, is_borrowed)) in iter.into_iter().enumerate() {
-            let got = remove_unwanted(input, remove);
-            let is_borrowed_got = match got {
-                Cow::Owned(_) => false,
-                Cow::Borrowed(_) => true,
-            };
-            if want != got
-            /* || is_borrowed != is_borrowed_got */
-            {
-                eprintln!("Test Index: {}\nInput: '{}'", index, input);
-                eprintln!("want: '{}', got: '{}'", want, got);
-                eprintln!("want: '{}', got: '{}'", is_borrowed, is_borrowed_got);
-            }
-            assert_eq!(want, got);
-            // assert_eq!(is_borrowed, is_borrowed_got); // TODO: fix bug
-        }
-    }
-    #[test]
-    fn test_remove_unwanted() {
-        let test = vec![
-            ("", vec![], "", true),
-            ("abc", vec![1], "ac", false),
-            ("abcd", vec![0], "bcd", true),
-            ("abcdefgh", vec![0, 1], "cdefgh", true),
-            ("abcdefgh", vec![0, 1, 2, 3], "efgh", true),
-            ("abcdefgh", vec![7], "abcdefg", true),
-            ("abcdefgh", vec![6, 7], "abcdef", true),
-            ("abcdefgh", vec![0, 1, 6, 7], "cdef", true),
-            ("abcdefgh", vec![0, 1, 3, 6, 7], "cef", false),
-            ("", vec![0], "", true),
-            ("abc", vec![10], "abc", true),
-            ("abc", vec![2, 3, 4], "ab", true),
-            ("abc", vec![1, 2, 3, 4], "a", true),
-        ];
-        test_fn_rm_unwanted(test);
-    }
-    fn test_fn_iter_args(test: Vec<(&str, Vec<&str>)>) {
-        for (index, (input, want)) in test.into_iter().enumerate() {
-            let args = IterArgs::new(input);
-            let got: Vec<Cow<'_, str>> = args.collect();
-            if got != want {
-                eprintln!("Test Index: {}", index);
-                eprintln!("want: {:?}, got: {:?}", want, got);
-            }
-            assert_eq!(want, got);
-        }
-    }
-    #[test]
-    fn test_iter_args() {
-        let test = vec![
-            ("hello", vec!["hello"]),
-            (r#"""""hello"""""#, vec!["hello"]),
-            ("hello world", vec!["hello", "world"]),
-            (r#"hello\ world"#, vec!["hello world"]),
-            (r#"   hello\ world   "#, vec!["hello world"]),
-            (
-                r#"   hello\ world  foo'bar'foo "#,
-                vec!["hello world", "foobarfoo"],
-            ),
-            (
-                "hello world 'hello   world'\\ ",
-                vec!["hello", "world", "hello   world "],
-            ),
-            (
-                r#"'/tmp/foo/"f 50"' '/tmp/foo/"f\68"' '/tmp/foo/f67'"#,
-                vec![
-                    r#"/tmp/foo/"f 50""#,
-                    r#"/tmp/foo/"f\68""#,
-                    r#"/tmp/foo/f67"#,
-                ],
-            ),
-        ];
-        test_fn_iter_args(test);
-    }
-
-    fn test_fn_cmd_match(iter: Vec<(&str, Cmd)>) {
-        for (index, (input, cmd)) in iter.into_iter().enumerate() {
-            let cmd_got = Cmd::from(input);
-            if cmd != cmd_got {
-                eprintln!("Test Index: {}", index);
-                eprintln!("want: '{}', got: '{}'", cmd, cmd_got);
-            }
-            assert_eq!(cmd, cmd_got);
-        }
-    }
-    #[test]
-    fn test_cmd_from_str<'a>() {
-        let fn_into_cow = |input: Vec<&'a str>| -> Vec<Cow<'a, str>> {
-            input.into_iter().map(|v| Cow::Borrowed(v)).collect()
-        };
-        let test = vec![
-            (
-                "echo hello world",
-                Cmd::Echo(fn_into_cow(vec!["hello", "world"])),
-            ),
-            (
-                r#"cat '/tmp/foo/"f 50"' '/tmp/foo/"f\68"' '/tmp/foo/f67'"#,
-                Cmd::Cat(fn_into_cow(vec![
-                    r#"/tmp/foo/"f 50""#,
-                    r#"/tmp/foo/"f\68""#,
-                    r#"/tmp/foo/f67"#,
-                ])),
-            ),
-            ("type ls", Cmd::Type("ls".into())),
-        ];
-        test_fn_cmd_match(test);
-    }
 }
